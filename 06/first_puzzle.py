50802353eea1a525b0a2059b610506f511fd3740
Felix Schulze Sindern <felixs@fh-muenster.de>
2021-12-06 19:07:53 +0100

    Solve day 6

--- 06/first_puzzle.py
+++ 06/first_puzzle.py
@@ -1,3 +1,27 @@
-lines = open("example-input.txt", "r").readlines()
-# lines = open("input.txt", "r").readlines()
+from typing import List
 
+
+# lines = open("example-input.txt", "r").readlines()
+lines = open("input.txt", "r").readlines()
+
+
+def parse_input(line: str) -> List[int]:
+    return list(map(int, line.split(",")))
+
+
+def simulate_day(fishes: List[int]) -> List[int]:
+    aged_fishes = list(map(lambda fish: fish - 1, fishes))
+    number_of_new_fishes = len(list(filter(lambda fish: fish == -1, aged_fishes)))
+    aged_fishes_and_new_fishes = aged_fishes
+    aged_fishes_and_new_fishes.extend([8] * number_of_new_fishes)
+    return list(map(lambda fish: 6 if fish == -1 else fish, aged_fishes_and_new_fishes))
+
+
+fishes = parse_input(lines[0])
+
+print(f"Initial state: {fishes}")
+for i in range(80):
+    fishes = simulate_day(fishes)
+    # print(f"After {i+1} days: {fishes}")
+
+print(len(fishes))
--- 06/second_puzzle.py
+++ 06/second_puzzle.py
@@ -1,2 +1,31 @@
-lines = open("example-input.txt", "r").readlines()
-# lines = open("input.txt", "r").readlines()
+from typing import List
+
+
+# lines = open("example-input.txt", "r").readlines()
+lines = open("input.txt", "r").readlines()
+
+
+def parse_input(line: str):
+    fishes_init = list(map(int, line.split(",")))
+    fishes = [0] * 9
+    for i in range(9):
+        fishes[i] = len(list(filter(lambda fish: fish == i, fishes_init)))
+    return fishes
+
+
+def simulate_day(fishes: List[int]):
+    number_of_fishes_at_zero = fishes[0]
+    fishes.pop(0)
+    fishes.append(0)
+    fishes[6] += number_of_fishes_at_zero
+    fishes[8] += number_of_fishes_at_zero
+    return fishes
+
+
+fishes = parse_input(lines[0])
+
+print(f"Initial state: {fishes}")
+for i in range(256):
+    fishes = simulate_day(fishes)
+
+print(sum(fishes))
